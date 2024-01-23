f3e6318cd0908836ac51c1e9911e3e719b9f021a
diogobnfr <diogobonofre@gmail.com>
2024-01-23 03:39:41 -0300

    Added word_size() and random_word() functions. - Several bugs spotted! - Added words.txt

--- main.c
+++ main.c
@@ -2,17 +2,27 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
-#define LETTERS_SIZE 50
+#define BUFFER_SIZE 50
 
 typedef struct {
-  char data[50];
+  char data[BUFFER_SIZE];
   int size;
 } LETTERS;
 
 void init_letters(LETTERS* g) {
-  for (int i = 0; i < LETTERS_SIZE; i++) g->data[i] = '_';
+  for (int i = 0; i < BUFFER_SIZE; i++) g->data[i] = '_';
   g->size = 0;
+}
+
+int word_size(char* buffer) {
+  int size = 0;
+  for (int i = 0; i < BUFFER_SIZE; i++) {
+    if (buffer[i] == '\0') break;
+    size++;
+  }
+  return size;
 }
 
 bool check_guess(char guess, const char* word, LETTERS* letters) {
@@ -40,6 +50,37 @@ void print_letters(LETTERS* l, int word_size) {
     printf("%c ", l->data[i]);
   }
   printf("\n");
+}
+
+int random_word(char* filename, char* buffer) {
+  FILE* file = fopen(filename, "r");
+
+  if (file == NULL) {
+    printf("Error: file not found.");
+    return 1;
+  }
+
+  srand(time(NULL));
+
+  // TODO: make reader adaptable to different line numbers files
+  int read_line = rand() % 46 + 1;
+  int current_line = 1;
+
+  do {
+    if (fgets(buffer, sizeof(buffer), file) == NULL) {
+      printf("Error: can't read line %d\n", read_line);
+      fclose(file);
+      return 1;
+    }
+
+    if (current_line == read_line) {
+      break;
+    }
+
+    current_line++;
+  } while (true);
+
+  return 0;
 }
 
 void draw_stickman(int errors) {
@@ -84,36 +125,43 @@ void draw_stickman(int errors) {
 }
 
 int main(int argc, char const* argv[]) {
-  char word[] = "create";
+  char buffer[BUFFER_SIZE];
   int errors = 0;
   char* guess = (char*)malloc(sizeof(char));
+
+  // TODO: make all letter lowercase (file/user input)
+  random_word("words.txt", buffer);
+  int size = word_size(buffer);
+  printf("%s\n", buffer);
 
   LETTERS letters;
   init_letters(&letters);
 
   draw_stickman(errors);
-  print_letters(&letters, sizeof(word));
+  print_letters(&letters, size);
   printf("Errors: %d\n", errors);
 
   while (true) {
     printf("Guess: ");
     scanf(" %c", guess);
 
-    if (!check_guess(*guess, word, &letters)) errors++;
+    if (!check_guess(*guess, buffer, &letters)) errors++;
 
     draw_stickman(errors);
-    print_letters(&letters, sizeof(word));
+    print_letters(&letters, size);
     printf("Errors: %d\n", errors);
 
     if (errors == 3) {
       printf("YOU LOSE!\n");
       break;
-    }
-    if (check_letters(&letters, word)) {
+    }  // TODO: don't recognize win
+    if (check_letters(&letters, buffer)) {
       printf("YOU WIN!\n");
       break;
     }
   }
+
+  printf("The word was: %s", buffer);
 
   return 0;
 }
